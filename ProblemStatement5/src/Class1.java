714af6f6f37ea2d64eabae5092d7ec5a6dcf17f0
SruthiAmmu2 <110349740+SruthiAmmu2@users.noreply.github.com>
2023-08-02 22:14:01 +0100

    Problem Statement 6

---
+++ ProblemStatement5/src/Class1.java
@@ -0,0 +1,21 @@
+
+public class Class1 {
+
+	String num;
+
+	public Class1(int a) 
+	{
+		num = "Constructor which takes integer value " +a;
+	}
+
+	public Class1(float a) 
+	{
+		num = "Constructor which takes float value " +a;
+	}
+
+	public Class1() 
+	{
+		num = "Constructor without any attribute";
+	}
+	
+}
---
+++ ProblemStatement5/src/Constructors1.java
@@ -0,0 +1,18 @@
+
+public class Constructors1 {
+
+	
+	public static void main(String[] args) {
+		
+		Class1 obj = new Class1(2);
+		System.out.println(obj.num);
+		
+		Class1 obj1 = new Class1(2.5f);
+		System.out.println(obj1.num);
+		
+		Class1 obj3 = new Class1();
+		System.out.println(obj3.num);
+
+	}
+
+}
